22d699f5def2b5e5b3fbd73a8859bec0145f9611
Lev <elchaninovlev31@gmail.com>
2026-07-05 00:45:46 +0300

    Added some tests (Сlear, Add_edge, Remove_edge)

--- tests/test_all.cpp
+++ tests/test_all.cpp
@@ -27,9 +27,12 @@ TEST_F(Graph_test, Add_vertex) {
     EXPECT_NO_THROW(graph_.get_vertex("PP"));
     EXPECT_EQ(graph_.get_vertex("PP").get_id(), "PP");
     EXPECT_EQ(graph_.get_vertex("PP").get_outgoing_edges().size(), 0);
+
+
+
 }
 
-TEST_F(Graph_test, Delete_vertex) {
+TEST_F(Graph_test, Remove_vertex) {
     EXPECT_NO_THROW(graph_.get_vertex("D"));
     EXPECT_NO_THROW(graph_.get_vertex("B").get_outgoing_edge("D"));
     EXPECT_NO_THROW(graph_.get_vertex("C").get_outgoing_edge("D"));
@@ -39,9 +42,56 @@ TEST_F(Graph_test, Delete_vertex) {
     EXPECT_THROW(graph_.get_vertex("B").get_outgoing_edge("D"),std::out_of_range);
     EXPECT_THROW(graph_.get_vertex("C").get_outgoing_edge("D"),std::out_of_range);
     EXPECT_THROW(graph_.get_vertex("D"), std::invalid_argument);
+
+    auto error_msg = graph_.delete_vertex("NON_EXIST");
+    EXPECT_EQ(error_msg, "Unknown node NON_EXIST");
 }
 
+TEST_F(Graph_test, Clear) {
+    EXPECT_NE(graph_.get_vertices().size(), 0);
+    graph_.clear();
+    EXPECT_EQ(graph_.get_vertices().size(), 0);
+}
 
+TEST_F(Graph_test, Add_edge) {
+    graph_.add_vertex("T1");
+    graph_.add_vertex("T2");
+    EXPECT_THROW(graph_.get_edge("T1", "T2"), std::out_of_range);
+    EXPECT_NO_THROW(graph_.get_edge("A", "B"));
+    EXPECT_THROW(graph_.get_edge("A", "T1"), std::out_of_range);
+
+    graph_.add_edge(111, "T1", "T2");
+    EXPECT_NO_THROW(graph_.get_edge("T1", "T2"));
+    auto current_edge = graph_.get_edge("T1", "T2");
+    EXPECT_EQ(current_edge.get_dest().get_id(), "T2");
+    EXPECT_EQ(current_edge.get_source().get_id(), "T1");
+    EXPECT_EQ(current_edge.get_weight(), 111);
+
+    graph_.add_edge(999, "T2", "T1");
+    EXPECT_THROW(graph_.get_edge("T2", "T1"), std::out_of_range);
+
+    EXPECT_NO_THROW(graph_.add_edge(-1, "A", "E"));
+    EXPECT_NO_THROW(graph_.add_edge(0, "A", "D"));
+
+    auto error_msg = graph_.add_edge(100, "A", "NON_EXIST");
+    EXPECT_EQ(error_msg, "Unknown node NON_EXIST");
+
+    error_msg = graph_.add_edge(100, "NON_EXIST_1", "NON_EXIST_2");
+    EXPECT_EQ(error_msg, "Unknown nodes NON_EXIST_1 NON_EXIST_2");
+}
+
+TEST_F(Graph_test, Remove_edge) {
+    EXPECT_NO_THROW(graph_.get_edge("A", "B"));
+    graph_.delete_edge("A", "B");
+    EXPECT_THROW(graph_.get_edge("A", "B"), std::out_of_range);
+
+    graph_.add_edge(1000, "A", "A");
+    EXPECT_NO_THROW(graph_.get_edge("A", "A"));
+    graph_.delete_edge("A", "A");
+    EXPECT_THROW(graph_.get_edge("A", "A"), std::out_of_range);
+
+    EXPECT_NO_THROW(graph_.delete_edge("NON_EXIST_1", "NON_EXIST_2"));
+}
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
