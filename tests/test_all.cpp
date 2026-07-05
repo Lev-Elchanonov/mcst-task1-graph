57f1284b5040734b5f6a81ef51092362ce4f973a
Lev <elchaninovlev31@gmail.com>
2026-07-05 11:50:46 +0300

    Finished writing tests for maxflow

--- tests/test_all.cpp
+++ tests/test_all.cpp
@@ -174,6 +174,63 @@ TEST_F(Graph_test, Maxflow) {
     res = maxflow->execute(graph_, {"A", "E"});
     EXPECT_EQ(res, "90");
 
+    //////////////////////////////////////////////////////
+    graph_.clear();
+    graph_.add_vertex("A1");
+    graph_.add_vertex("A2");
+    graph_.add_vertex("A3");
+    graph_.add_vertex("A4");
+    graph_.add_vertex("A5");
+    graph_.add_vertex("A6");
+    graph_.add_vertex("A7");
+    graph_.add_vertex("A8");
+
+    graph_.add_edge(95, "A1", "A3");
+    graph_.add_edge(32, "A1", "A2");
+    graph_.add_edge(75, "A1", "A4");
+    graph_.add_edge(57, "A1", "A5");
+    graph_.add_edge(23, "A2", "A5");
+    graph_.add_edge(16, "A2", "A8");
+    graph_.add_edge(5, "A2", "A3");
+    graph_.add_edge(18, "A3", "A4");
+    graph_.add_edge(6, "A3", "A6");
+    graph_.add_edge(9, "A4", "A6");
+    graph_.add_edge(24, "A4", "A5");
+    graph_.add_edge(20, "A5", "A7");
+    graph_.add_edge(94, "A5", "A8");
+    graph_.add_edge(11, "A6", "A5");
+    graph_.add_edge(7, "A6", "A7");
+    graph_.add_edge(81, "A7", "A8");
+    res = maxflow->execute(graph_, {"A1", "A8"});
+    EXPECT_EQ(res, "128");
+
+    //////////////////////////////////////////////////////
+    graph_.clear();
+    graph_.add_vertex("A1");
+    graph_.add_vertex("A2");
+    graph_.add_vertex("A3");
+    graph_.add_vertex("A4");
+    graph_.add_vertex("A5");
+    graph_.add_vertex("A6");
+    graph_.add_vertex("A7");
+    graph_.add_vertex("A8");
+
+    graph_.add_edge(5, "A1", "A2");
+    graph_.add_edge(40, "A1", "A3");
+    graph_.add_edge(5, "A1", "A4");
+    graph_.add_edge(5, "A2", "A5");
+    graph_.add_edge(30, "A3", "A2");
+    graph_.add_edge(30, "A3", "A4");
+    graph_.add_edge(20, "A3", "A6");
+    graph_.add_edge(5, "A4", "A7");
+    graph_.add_edge(15, "A5", "A8");
+    graph_.add_edge(5, "A6", "A5");
+    graph_.add_edge(5, "A6", "A7");
+    graph_.add_edge(5, "A6", "A8");
+    graph_.add_edge(15, "A7", "A8");
+    res = maxflow->execute(graph_, {"A1", "A8"});
+    EXPECT_EQ(res, "25");
+
 }
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
